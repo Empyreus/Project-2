f170aa839e42bd8eeaf37f763e2a53f7756ea436
rjsouza1995 <magestic1995@gmail.com>
2016-03-01 12:29:03 -0800

    First RandomBST Run

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,23 +1,42 @@
-
 public class BinarySearchTree {
     BSTNode root;
-    public BinarySearchTree(){
+    boolean printFlag;
+
+    public BinarySearchTree() {
         root = null;
+        printFlag = false;
     }
-    public BSTNode getRoot(){
+
+    public BSTNode getRoot() {
         return root;
     }
-    public void insert(int x){
-        root = insert(x,root);
+
+    public void insert(int x) {
+        root = insert(x, root);
     }
-    public void setRoot(int x){
+
+    public void setRoot(int x) {
         BSTNode temp = new BSTNode(x);
         root = temp;
     }
-    public int getHeight(){
+
+    public boolean search(int x) {
+        return search(x, root);
+    }
+
+    public int getHeight() {
         return root.getHeight();
     }
-    
+
+    public void setPrintFlag(boolean x) {
+        printFlag = x;
+    }
+
+    public void removeRoot() {
+        if (root != null)
+            root = remove(root.element, root);
+    }
+
     /**
      * Internal method to insert into a subtree.
      * 
@@ -29,8 +48,7 @@ public class BinarySearchTree {
      */
     private BSTNode insert(int x, BSTNode t) {
         if (t == null)
-            //return new BSTNode(x, null, null);
-            setRoot(x);
+            return new BSTNode(x, null, null);
         if (x < t.element)
             t.left = insert(x, t.left);
         else if (x > t.element)
@@ -38,5 +56,56 @@ public class BinarySearchTree {
         else
             ; // Duplicate; do nothing
         return t;
+    }
+
+    /**
+     * Internal method to remove from a subtree.
+     * 
+     * @param x
+     *            the item to remove.
+     * @param t
+     *            the node that roots the subtree.
+     * @return the new root of the subtree.
+     */
+    private BSTNode remove(int x, BSTNode t) {
+        if (t == null)
+            return t; // Item not found; do nothing
+
+        if (x < t.element)
+            t.left = remove(x, t.left);
+        else if (x > t.element)
+            t.right = remove(x, t.right);
+        else if (t.left != null && t.right != null) // Two children
+        {
+            t.element = findMin(t.right).element;
+            t.right = remove(t.element, t.right);
+        } else
+            t = (t.left != null) ? t.left : t.right;
+        return t;
+    }
+
+    private boolean search(int x, BSTNode t) {
+        if (t == null)
+            return false;
+        if (x < t.element)
+            return search(x, t.left);
+        if (x > t.element)
+            return search(x, t.right);
+        return true;
+    }
+
+    /**
+     * Internal method to find the smallest item in a subtree.
+     * 
+     * @param t
+     *            the node that roots the subtree.
+     * @return node containing the smallest item.
+     */
+    private BSTNode findMin(BSTNode t) {
+        if (t == null)
+            return null;
+        else if (t.left == null)
+            return t;
+        return findMin(t.left);
     }
 }
---
+++ src/RandomBST.java
@@ -0,0 +1,128 @@
+import java.util.Random;
+
+public class RandomBST {
+    BinarySearchTree BST;
+
+    public RandomBST(boolean printFlag) {
+        BST = new BinarySearchTree();
+        BST.setPrintFlag(printFlag);
+    }
+
+    /*
+     * Populate tree with 35 random integers range : 10 -99
+     */
+    public void populate() {
+        final int number = 35, min = 10, max = 99;
+        final int mFactor = (max - min + 1);
+        for (int i = 0; i < number; i++) {
+            int rn = (int) (Math.random() * mFactor + min);
+            BST.insert(rn);
+            printTree("Tree after inserting random number: " + rn);
+        }
+    }
+
+    public void remove() {
+        while (BST.getHeight() >= 0) {
+            BST.removeRoot();
+            printTree("Tree after deleting root:");
+        }
+    }
+
+    public void printTree(String label) {
+        BSTPrinter tp = new BSTPrinter(BST);
+        tp.print(label);
+    }
+
+    /*
+     * Generate random numbers use seed to get same set of numbers
+     */
+    public static int[] generateRandomInt(int num, long seed) {
+        int[] numbers = new int[num];
+        Random rg = new Random(seed);
+        for (int i = 0; i < num; ++i) {
+            numbers[i] = rg.nextInt();
+        }
+        return numbers;
+    }
+
+    public void performInsertion(int[] numbers) {
+        int len = numbers.length;
+        for (int i = 0; i < len; ++i) {
+            BST.insert(numbers[i]);
+        }
+    }
+
+    public void performSearch(int[] numbers) {
+        int len = numbers.length;
+        for (int i = 0; i < len; ++i) {
+            BST.search(numbers[i]);
+        }
+    }
+
+    public void performMixture(int[] insertNumbers, int[] searchNumbers) {
+        performInsertion(insertNumbers);
+        performSearch(searchNumbers);
+    }
+
+    public static void main(String[] args) {
+        boolean printFlag = true;
+        RandomBST ra = new RandomBST(printFlag);
+        // Part 1
+
+        ra.populate();
+        ra.remove();
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        // Part2
+        printFlag = false;
+        final int k = 5000;
+        long seed = k; // Seed is same as number of random integers to generate
+        final int[] kNumbers = generateRandomInt(k, seed);
+        int num = 1000;
+        for (int i = 0; i < 4; ++i) {
+            System.out.println("");
+            System.out.println("");
+            seed = num; // Seed as number
+            int[] numbers = generateRandomInt(num, seed);
+            RandomBST test_ra = new RandomBST(printFlag);
+
+            long start = System.nanoTime();
+            test_ra.performInsertion(numbers);
+            long end = System.nanoTime();
+            System.out.println("Tavl(" + num + ") = " + (end - start)
+                    + " nanoseconds");
+
+            // Search
+            start = System.nanoTime();
+            test_ra.performSearch(kNumbers);
+            end = System.nanoTime();
+            System.out.println("Tavl_search(" + num + "," + k + ") = "
+                    + (end - start) + " nanoseconds");
+
+            // Mixture of insertions and searches.
+            int insertFactor = 1 + i;
+            int searchFactor = 1;
+
+            int numInsertions = insertFactor * k;
+            int numSearches = searchFactor * k;
+            seed = numInsertions;
+            int[] insertionNumbers = generateRandomInt(numInsertions, seed);
+            seed = numSearches;
+            int[] searchNumbers = generateRandomInt(numSearches, seed);
+
+            start = System.nanoTime();
+            test_ra.performMixture(insertionNumbers, searchNumbers);
+            end = System.nanoTime();
+            System.out.println("Insertion:Search = " + insertFactor + ":"
+                    + searchFactor);
+            System.out.println("Tavl_mixture(" + num + "," + numInsertions
+                    + "," + numSearches + ") = " + (end - start)
+                    + " nanoseconds");
+            num = num * 10;
+        }
+    }
+}
